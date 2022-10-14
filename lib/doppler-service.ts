b925c198f4ee61ceda9185d7c88a513b56572f23
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-15 00:37:38 +0200

    clone doppler-service and switch new one to directly using axios

--- lib/doppler-service.ts
+++ lib/doppler-service.ts
@@ -1,13 +1,7 @@
 // deno-lint-ignore-file require-await
 // import retrofit from "ts-retrofit";
-import {
-  BasePath,
-  BaseService,
-  GET,
-  Query,
-  Response,
-  ServiceBuilder,
-} from "npm:ts-retrofit@1.18.0";
+import axios from "redaxios";
+import { encode as encodeBase64 } from "std_base64";
 import { Project } from "./types/Project.ts";
 import { Environment } from "./types/Environment.ts";
 
@@ -15,40 +9,24 @@ export type DopplerServiceOptions = {
   token: string;
 };
 
-export const BASE_URL = "https://api.doppler.com";
+export const BASE_URL = "https://api.doppler.com/v3";
 
-@BasePath("/v3")
-export class DopplerService extends BaseService {
-  @GET("/projects")
-  async getProjects(): Promise<Response<Project[]>> {
-    return <Response<Project[]>> {};
+export class DopplerService {
+  private http;
+
+  constructor({ token }: DopplerServiceOptions) {
+    this.http = axios.create({
+      baseURL: BASE_URL,
+      // auth: encodeBase64(`${token}:dummy_password`),
+      auth: encodeBase64(`${token}:dummy_password`),
+    });
   }
 
-  @GET("/environments")
-  async getEnvironments(
-    @Query("project") project: string,
-  ): Promise<Response<Environment[]>> {
-    return <Response<Environment[]>> {};
+  async getProjects() {
+    return this.http.get(`/projects`);
   }
 
-  /**
-   * @param token The token used for authenticating against doppler => https://docs.doppler.com/reference/auth-token-formats
-   * @returns DopplerService
-   */
-  static create({ token }: DopplerServiceOptions) {
-    const dopplerInstance = new ServiceBuilder()
-      .setEndpoint(BASE_URL)
-      .setStandalone(true)
-      .setRequestInterceptors((config) => {
-        config.auth = {
-          username: token,
-          password: "",
-        };
-        return config;
-      })
-      // .setResponseInterceptors(ResponseInterceptor)
-      .build(DopplerService);
-
-    return dopplerInstance;
+  async getEnvironments(project: string) {
+    return this.http.get(`/environments`, { params: { project } });
   }
 }
---
+++ lib/doppler-service.ts-retrofit.ts
@@ -0,0 +1,57 @@
+// deno-lint-ignore-file require-await
+// import retrofit from "ts-retrofit";
+import {
+  BasePath,
+  BaseService,
+  Body,
+  GET,
+  Query,
+  Response,
+  ServiceBuilder,
+} from "npm:ts-retrofit@1.18.0";
+import { Project } from "./types/Project.ts";
+import { Environment } from "./types/Environment.ts";
+
+export type DopplerServiceOptions = {
+  token: string;
+};
+
+export const BASE_URL = "https://api.doppler.com";
+
+@BasePath("/v3")
+export class DopplerService extends BaseService {
+  @GET("/projects")
+  async getProjects(
+    @Body() body: string | undefined,
+  ): Promise<Response<Project[]>> {
+    return <Response<Project[]>> {};
+  }
+
+  @GET("/environments")
+  async getEnvironments(
+    @Query("project") project: string,
+  ): Promise<Response<Environment[]>> {
+    return <Response<Environment[]>> {};
+  }
+
+  /**
+   * @param token The token used for authenticating against doppler => https://docs.doppler.com/reference/auth-token-formats
+   * @returns DopplerService
+   */
+  static create({ token }: DopplerServiceOptions) {
+    const dopplerInstance = new ServiceBuilder()
+      .setEndpoint(BASE_URL)
+      .setStandalone(true)
+      .setRequestInterceptors((config) => {
+        config.auth = {
+          username: token,
+          password: "",
+        };
+        return config;
+      })
+      // .setResponseInterceptors(ResponseInterceptor)
+      .build(DopplerService);
+
+    return dopplerInstance;
+  }
+}
