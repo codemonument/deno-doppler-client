8fe6c57548838b65fa5985773355078689eaf92d
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-14 23:41:26 +0200

    add project listing in main.ts example

--- example/main.ts
+++ example/main.ts
@@ -1,9 +1,15 @@
-import {} from "/mod.ts";
+import { DopplerService } from "@/mod.ts";
+import { DOPPLER_TOKEN } from "@/env.ts";
 import { log } from "std_log";
-import { VERSION } from "/version.ts";
+import { VERSION } from "@/version.ts";
 
 try {
   log.info(`Module Version (version.ts): ${VERSION}`);
+
+  const service = DopplerService.create({ token: DOPPLER_TOKEN });
+  const projects = await service.getProjects();
+
+  console.log(projects);
 } catch (error) {
   console.error(error);
   Deno.exit();
