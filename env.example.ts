304cf940b475c0be98f0d8d7797e17eca37ceafa
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-14 23:37:11 +0200

    add env.example.ts file

---
+++ env.example.ts
@@ -0,0 +1,6 @@
+/**
+ * 1. Acquire a service token: https://docs.doppler.com/docs/service-tokens
+ * 2. Paste that token here
+ * 3. Rename that file to env.ts
+ */
+export const DOPPLER_TOKEN = "";
